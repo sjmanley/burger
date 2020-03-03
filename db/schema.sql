3ea925d70c5f5820e1998a029475730ab42a9dba
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-03-02 18:18:11 -0600

    new files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+create database burgers_db;
+
+    use burgers_db
+
+    create table (
+        id primary key,
+        burger_name VARCHAR (30),
+        devoured BOOLEAN NOT NULL
+    );
+
+    
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers_db (column1, column2, column3)  
+VALUES (value1, value2, value3,...valueN);

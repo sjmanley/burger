e861d32e65b797c4986d7ffe7af1ba18df4d9f31
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-03-10 19:55:36 -0500

    working files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,6 @@
 INSERT INTO burgers_db (column1, column2, column3)  
 VALUES (value1, value2, value3,...valueN);
+
+-- INSERT INTO burgers (name, eaten) VALUES ('Bacon Double Cheese', false);
+-- INSERT INTO burgers (name, eaten) VALUES ('Chicken and Mayo', false);
+-- INSERT INTO burgers (name, eaten) VALUES ('Cuban extra Pickles', false);
